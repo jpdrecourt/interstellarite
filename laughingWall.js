55cc12d8581a4c0641660da2d889976b3889b218
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2016-07-25 17:55:03 +0100

    Semi-auto loading of files

--- laughingWall.js
+++ laughingWall.js
@@ -1,25 +1,52 @@
+
+
 class VideoWall {
   constructor (width=150) {
-    let videoName = [];
-    videoName[0] = './clips.nobackup/gmleHMY3OOo_1.mp4';
-    videoName[1] = './clips.nobackup/gmleHMY3OOo_2.mp4';
+    let videoNames = createVideoNames();
+    let nVideos = 0, maxVideos = 10;
 
-    for (let i = 0; i < 8; i++) {
-      $('body').append('<div class="videoLine"></div>');
-      for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 10; i++) {
+      $('.wrapper').append('<div class="videoLine"></div>');
+      for (let j = 0; j < 10; j++) {
         $('.videoLine').last().append('<div class="videoDiv"></div>');
         $('.videoDiv').last().append('<video></video>');
         $('video').last().attr({
-          'src': videoName[(i+j)%2],
+          'src': videoNames[(i+j)%videoNames.length],
           'control': false,
-          'width': '150px',
-          'onmouseover': 'if (this.paused) {this.play(); console.log("Played")}'
+          'width': '100px'
         });
       }
     }
+    $('body').mouseover (function(e) {
+      let target = $(e.target);
+      if (nVideos < maxVideos && target.is('video') && target.get(0).paused) {
+        nVideos++;
+        target.get(0).play();
+      }
+    });
 
+    $('video').on('ended', function() {
+      nVideos--;
+    });
 
   }
+}
+
+
+function createVideoNames() {
+  let clipData = {
+    'rootDir': './clips.nobackup/',
+    'clips': [
+      {'name': 'gmleHMY3OOo', n: 8}
+    ]
+  };
+  let output = [];
+  for (let i = 0, l = clipData.clips.length; i < l; i++) {
+    for (let j = 0; j < clipData.clips[i].n; j++) {
+      output.push(clipData.rootDir + clipData.clips[i].name + '_' + (j + 1) + '.mp4');
+    }
+  }
+  return output;
 }
 
 $(document).ready(() => {
